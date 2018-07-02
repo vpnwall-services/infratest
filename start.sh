a2be3b5fbd90e375f4441458256046e34148532d
majes <radafk@gmail.com>
2018-07-02 23:11:38 +0200

    updated start.sh 2

--- start.sh
+++ start.sh
@@ -13,7 +13,9 @@
 
 deployment_to_swarm(){
 cat << EOF > /tmp/$WEbSITE_NAME.docker.yaml
-docker swarm init
+docker swarm init --advertise-addr $IP_ADDRESS
+docker swarm join --token $TOKEN:2377
+docker service create --name $WEBSITE_NAME --publish 443:443 --replicas $REPLICA_NUMBER localhost:5000/example.com.template
 EOF
 }
 
@@ -26,7 +28,7 @@ echo -e "Say you launch this script with example.com as parameter"
 echo -e "like : ./start.sh example.com"
 echo -e "you must have a example.com folder where start.sh resides"
 echo -e "and have two files in that folder"
-echo -e "a plugins.txt file with all the plugins names from the previous install"
+echo -e "A plugins.txt file with all the plugins names from the previous install"
 echo -e "and a requirements.txt file where all ubuntu depandancies and their versions"
 echo -e "are listed and ip address of AWS EC2 instance, one line for each for both files"
 echo -e "here's an example of requirements.txt file"
@@ -38,6 +40,9 @@ echo -e "mysql-client  "
 echo -e "mysql-server  "
 echo -e "IP_ADDRESS=192.168.0.1"
 echo -e "-----------------------------------------------------"
+echo -e "You also have to have a ubuntu docker template image"
+echo -e "like localhost:5000/example.com.template"
+
 
 echo -e "Example: ./start.sh example.com"
 echo -e "with a example.com folder"
